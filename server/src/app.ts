c17389eb909743f513fea7b1e21e1854cba677e0
anandhu-as <anandhuas010203@gmail.com>
2026-06-05 08:36:28 +0530

    first commit

---
+++ server/src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import {PORT} from "./config/env.js"
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the subscription tracker api");
+});
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
