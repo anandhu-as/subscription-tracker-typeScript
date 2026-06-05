0679ae6ed40f37e5263a519cfdb23aa64af2dc35
anandhu-as <anandhuas010203@gmail.com>
2026-06-05 10:52:25 +0530

    added db_URI

---
+++ server/src/database/mongodb.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_URI, NODE_ENV } from "../config/env.js";
+
+if (!DB_URI) {
+  throw new Error("define the MONGODB_URI in .env");
+}
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(DB_URI!);
+    console.log(`connected to db in ${NODE_ENV} mode`);
+  } catch (error) {
+    console.log("error connecting to database", error);
+  }
+};
+export default connectToDatabase;
