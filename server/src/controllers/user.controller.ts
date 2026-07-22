6fb4c115e0223d87eef4ba1d0e20937eed434560
anandhu-as <anandhuas010203@gmail.com>
2026-07-22 11:45:26 +0530

    edited user-controller

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,16 +1,30 @@
 import User from "../dbmodels/user.model";
 import { Request, Response, NextFunction } from "express";
 
-export const getUsers = async (request: Request, response: Response, next: NextFunction) => {
+export const getUsers = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
   try {
     const users = await User.find();
-    response.status(200).json({ success: true, data: users });
+    if (users.length < 1) {
+      response
+        .status(404)
+        .json({ message: "User not found create user first!" });
+    } else {
+      response.status(200).json({ success: true, data: users });
+    }
   } catch (error) {
     next(error);
   }
 };
 
-export const getUserById = async (request: Request, response: Response, next: NextFunction) => {
+export const getUserById = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
   try {
     const user = await User.findById(request.params.id).select("-password");
     if (!user) {
